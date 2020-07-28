090fca1e124a1581a6167886c3493b157698c65f
Camilo Lopez <johan.lopez@bluetab.net>
2020-07-28 14:17:18 -0500

    First commit and and SpringBoot skeleton added

---
+++ src/test/java/io/johancamilo/springbootbasics/SpringbootBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.johancamilo.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
